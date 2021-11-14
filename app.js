64bfafeb3893403d3433d92d3492b48d26f7fd37
agosmac <agosmac@hotmail.com>
2021-11-14 18:44:22 -0300

    agregando formularios de register y login

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ app.use(express.static('public'));
 
 app.get("/", (req, res) => {
         res.sendFile(__dirname + '/views/index.html')
-    });
+});
+
+app.get("/register", (req, res) => {
+        res.sendFile(__dirname + "/views/register.html")
+});
+
+app.get("/login", (req, res) => {
+        res.sendFile(__dirname + "/views/login.html")
+}); 
 
     app.listen(process.env.PORT || 3000, () => {console.log('servidor funcionando')});
